d0405c223775fe3274fbea6180cbf8891918567e
voltyea <volty764@gmail.com>
2025-12-13 21:40:47 +0530

    removed unused files

--- dotfiles/quickshell/networkMonitor.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-
-dbus-monitor --system "type='signal',sender='org.freedesktop.NetworkManager'" 2>/dev/null |
-  while read -r line; do
-    if echo "$line" | grep -q -E "AccessPointAdded|AccessPointRemoved|LastScan|Strength|WirelessEnabled"; then
-      echo "updated!"
-    fi
-  done
