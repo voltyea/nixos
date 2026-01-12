5850fbbcdcacd00f86e1f65474167e210e3da6d3
Volty <volty764@gmail.com>
2026-01-12 18:21:45 +0530

    Change phase angle update method in Moon class
    Refactor Moon class to update phase angle correctly.

--- pkgs/luna/moon/moon.cpp
+++ pkgs/luna/moon/moon.cpp
@@ -3,13 +3,25 @@
 #include <QTimer>
 
 Moon::Moon(QObject *parent) : QObject(parent) {
-  auto *timer = new QTimer(this);
-  timer->setInterval(3600000);
-  timer->setTimerType(Qt::CoarseTimer);
-  connect(timer, &QTimer::timeout,
-      this, &Moon::phaseAngleChanged);
-  timer->start();
+    auto *timer = new QTimer(this);
+    timer->setInterval(3600000);
+    timer->setTimerType(Qt::CoarseTimer);
+    connect(timer, &QTimer::timeout,
+            this, &Moon::updatePhaseAngle);
+    timer->start();
+    updatePhaseAngle();
 }
+
 double Moon::phaseAngle() const {
-  return Astronomy_MoonPhase(Astronomy_CurrentTime()).angle;
+    return m_phaseAngle;
+}
+
+void Moon::updatePhaseAngle() {
+    const double newValue = Astronomy_MoonPhase(Astronomy_CurrentTime()).angle;
+
+    if (qFuzzyCompare(m_phaseAngle, newValue))
+        return;
+
+    m_phaseAngle = newValue;
+    emit phaseAngleChanged();
 }
