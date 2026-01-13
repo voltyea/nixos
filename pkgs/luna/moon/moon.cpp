2a525359d50bf4badd31633bd765951728a4c484
Volty <volty764@gmail.com>
2026-01-14 00:47:45 +0530

    Change signal connection to phaseAngle method

--- pkgs/luna/moon/moon.cpp
+++ pkgs/luna/moon/moon.cpp
@@ -4,7 +4,7 @@
 
 Moon::Moon(QObject *parent) : QObject(parent) {
   QTimer *timer = new QTimer(this);
-  connect(timer, &QTimer::timeout, this, &Moon::phaseAngleChanged);
+  connect(timer, &QTimer::timeout, this, &Moon::phaseAngle);
     timer->setInterval(3600000);
     timer->start();
 }
